84ec0a8775459dfa66dc6487416ff2a4ea5c0b14
[SW7] 정현지 <blackit14@gmail.com>
2024-01-11 15:13:13 +0900

    styledComponent #6: theme

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,50 +3,21 @@ import styled, { keyframes } from 'styled-components';
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>😆</Emoji>
-      </Box>
-      <Emoji>🔥</Emoji>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
 
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const rotateAnimation = keyframes`
-0% {
-  transform: rotate(0deg);
-  border-radius: 0px;
-}
-50% {
-  border-radius: 100px;
-}
-100%{
-  transform: rotate(360deg);
-  border-radius: 0px;
-}
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  animation: ${rotateAnimation} 1s linear infinite;
-  display: flex;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-
-  // 이렇게 하면 Emoji component가 p 이든, span 이든 상관없음
-  ${Emoji} {
-    &:hover {
-      font-size: 98px;
-    }
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+//Theme 사용하기 위한 패키지 import
+import { ThemeProvider } from 'styled-components';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const darkTheme = {
+  textColor: 'whitesmoke',
+  backgroundColor: '#111',
+};
+
+const lightTheme = {
+  textColor: '#111',
+  backgroundColor: 'whitesmoke',
+};
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
