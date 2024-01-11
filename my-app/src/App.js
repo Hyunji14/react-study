d4081b139fa42cb425d44a3600fca997ff3697d7
[SW7] 정현지 <blackit14@gmail.com>
2024-01-11 14:18:21 +0900

    styledComponent #2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,32 +3,24 @@ import styled from 'styled-components';
 function App() {
   return (
     <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo></BoxTwo>
+      <Box bgColor='teal'></Box>
+      <Cricle bgColor='whitesmoke'></Cricle>
     </Father>
   );
 }
-
-const Text = styled.span`
-  color: #fff;
-`;
 
 const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
-const BoxTwo = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
+const Cricle = styled(Box)`
+  border-radius: 50px;
 `;
 
 export default App;
