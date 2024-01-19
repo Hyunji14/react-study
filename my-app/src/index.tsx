4eedd833db68d4d1dd78251c829b1ab289bf926a
[SW7] 정현지 <blackit14@gmail.com>
2024-01-19 15:06:27 +0900

    Router #1 browserRouter

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-//Theme 사용하기 위한 패키지 import
-import { ThemeProvider } from 'styled-components';
-import { lightTheme, darkTheme } from './theme';
+// //Theme 사용하기 위한 패키지 import
+// import { ThemeProvider } from 'styled-components';
+// import { lightTheme, darkTheme } from './theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,8 +11,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <App />
-    </ThemeProvider>
+    {/* <ThemeProvider theme={darkTheme}> */}
+    <App />
+    {/* </ThemeProvider> */}
   </React.StrictMode>
 );
